ea8d0cf1abb93c1e46ae84affe75c0f4b99b3f8f
Pandi Selvi <sithanathanpandiselvi@gmail.com>
2022-06-27 03:15:46 +0530

    first commit

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> 98686 68686</label>
+            <i className='fa fa-envelope'></i>
+            <label> support@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <span>🏳️‍⚧️</span>
+            <label>EN</label>
+            <span>🏳️‍⚧️</span>
+            <label>USD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
